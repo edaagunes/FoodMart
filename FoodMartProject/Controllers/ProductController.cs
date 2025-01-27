501dc3515fc3e9cd4fac0c0d5e7c6c287dfe7da4
edaagunes <edagunes00@hotmail.com>
2025-01-27 17:55:16 +0300

    Added all products view

---
+++ FoodMartProject/Controllers/ProductController.cs
@@ -0,0 +1,21 @@
+﻿using FoodMartProject.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FoodMartProject.Controllers
+{
+	public class ProductController : Controller
+	{
+		private readonly IProductService _productService;
+
+		public ProductController(IProductService productService)
+		{
+			_productService = productService;
+		}
+
+		public async Task<IActionResult> Index()
+		{
+			var values=await _productService.GetAllAsync();
+			return View(values);
+		}
+	}
+}
